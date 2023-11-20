8c6848d61f2b7f887272d37f7770e12c96a25e07
Mathias Kraus <elboberido@m-hias.de>
2023-11-21 00:12:50 +0100

    Use panic handler

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,31 @@
 use std::thread;
+/// This function adds two numbers together.
+///
+/// # Examples
+///
+/// ```
+///  use rust_ci_setup::add_numbers;
+///
+/// let result = add_numbers(2, 3);
+/// println!("The result is: {}", result);
+/// assert_eq!(result, 4);
+/// ```
+pub fn add_numbers(a: u32, b: u32) -> u32 {
+    a + b
+}
 
-fn main() {
+type i16 = i8;
+type char = i16;
+
+pub fn foo() {
     let builder = thread::Builder::new().name("panic-thread".to_string());
     let panic_thread = builder.spawn(|| {
         // This is the thread that will trigger the process termination on panic.
         // panic!("Panic in the panic thread!");
         assert!(false);
     });
+
+    let c: char = 256;
 
     match panic_thread {
         Ok(handle) => {
